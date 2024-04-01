37edde29cab68b987947d667203c22c5b055d4ed
freudpunisher <punishergte@gmail.com>
2024-04-01 16:34:14 +0200

    create custom hoo to fetch the game

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,25 +1,12 @@
-import { useEffect, useState } from "react"
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
 
 
-interface Game {
-    id: number;
-    name:string;
-}
 
-interface FetchGamesResponse {
-    count: number;
-    results: Game[]; 
-}
 
 const GameGrid = () => {
-    const [ games, setGames] = useState<Game[]>([]);
-    const [ error, seterror] = useState('');
-useEffect(() => {
-    apiClient.get<FetchGamesResponse>('/games').then(res => setGames(res.data.results)).catch(error => seterror(error.message));
-})
+    const {games, error}=useGames();
 
   return (
     <div>
