506e2dce58d6e38a2145fc3291168c710a632ba7
freudpunisher <punishergte@gmail.com>
2024-04-02 17:50:25 +0200

    fix the space

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,7 +14,7 @@ const GameCard = ({game}: Props) => {
     // console.table(game.parent_plateforms)
 
   return (
-    <Card  borderRadius={10}  overflow='hidden'>
+    <Card borderRadius={10}  overflow='hidden'>
       <Image src={getCroppedImageUrl(game.background_image)}/>
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
