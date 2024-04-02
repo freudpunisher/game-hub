523e3620e39143b4dfd50a0acd8494d065c16534
freudpunisher <punishergte@gmail.com>
2024-04-02 11:49:45 +0200

    show loading skeleton loading

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,9 @@
 // import React from 'react'
 import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
 import PlatformIconlist from './PlatformIconlist'
+import CriticScore from './CriticScore'
+import getCroppedImageUrl from '../services/image-url'
 
 
 interface Props {
@@ -12,11 +14,15 @@ const GameCard = ({game}: Props) => {
     // console.table(game.parent_plateforms)
 
   return (
-    <Card borderRadius={10}  overflow='hidden'>
-      <Image src={game.background_image}/>
+    <Card width="300px" borderRadius={10}  overflow='hidden'>
+      <Image src={getCroppedImageUrl(game.background_image)}/>
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
+        <HStack justifyContent='space-between'>
+
         <PlatformIconlist plateforms={game.parent_platforms.map(p => p.platform)}/>
+        <CriticScore score={game.metacritic}/>
+        </HStack>
       </CardBody>
     </Card>
   )
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,6 +15,7 @@ export interface Game {
     name:string;
     background_image:string;
     parent_platforms: { platform: Platform}[];
+    metacritic: number; 
 }
 
 interface FetchGamesResponse {
@@ -25,16 +26,22 @@ interface FetchGamesResponse {
 const useGames = () => {
     const [ games, setGames] = useState<Game[]>([]);
     const [ error, seterror] = useState('');
+    const [ isLoading, setisLoading] = useState(false);
 useEffect(() => {
     const controller = new AbortController();
-    apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal}).then(res => setGames(res.data.results)).catch(error =>{
+    setisLoading(true);
+    apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal}).then(res => {setGames(res.data.results)
+        setisLoading(false);
+    }).catch(error =>{
         if(error instanceof CanceledError) return;
-         seterror(error.message)});
+        seterror(error.message)
+        setisLoading(false);
+    });
 
     return () => controller.abort();
 },[])
 
-return {games, error}
+return {games, error, isLoading}
 }
 
 export default useGames
